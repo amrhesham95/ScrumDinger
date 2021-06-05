ca32b72876d4aa899b912da852716e8ac1221427
Amr Hesham <amrhesham9595@gmail.com>
2021-06-05 18:08:41 +0200

    initial commit

---
+++ ScrumDinger/ScrumDinger/ScrumDingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumDingerApp.swift
+//  ScrumDinger
+//
+//  Created by Amr Hesham on 05/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct ScrumDingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
