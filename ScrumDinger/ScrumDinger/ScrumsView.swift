ea103fae843c40688ca5a75c3256c889e03b9c10
Amr Hesham <amrhesham9595@gmail.com>
2021-06-06 14:41:09 +0200

    Display Data in a list

---
+++ ScrumDinger/ScrumDinger/ScrumsView.swift
@@ -0,0 +1,30 @@
+//
+//  ScrumsView.swift
+//  ScrumDinger
+//
+//  Created by Amr Hesham on 06/06/2021.
+//
+
+import SwiftUI
+
+// MARK: - ScrumsView
+//
+struct ScrumsView: View {
+  let scrums: [DailyScrum]
+  var body: some View {
+    List {
+      ForEach(scrums, id: \.title) {
+        CardView(scrum: $0)
+          .listRowBackground($0.color)
+      }
+    }
+  }
+}
+
+// MARK: - ScrumtsView_Preview
+//
+struct ScrumtsView_Preview: PreviewProvider {
+  static var previews: some View {
+    ScrumsView(scrums: DailyScrum.data)
+  }
+}
