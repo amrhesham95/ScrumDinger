f9be80ca336a1152ca5c61381218172c0339afa9
Amr Hesham <amrhesham9595@gmail.com>
2021-06-06 15:00:49 +0200

    identifiy scrum

--- ScrumDinger/ScrumDinger/ScrumsView.swift
+++ ScrumDinger/ScrumDinger/ScrumsView.swift
@@ -13,7 +13,7 @@ struct ScrumsView: View {
   let scrums: [DailyScrum]
   var body: some View {
     List {
-      ForEach(scrums, id: \.title) {
+      ForEach(scrums) {
         CardView(scrum: $0)
           .listRowBackground($0.color)
       }
